eec6eb57b2215928282aee04fabf57e81a5f4f60
duanyunchao <duanyunchao@115.com>
2014-01-14 23:54:12 +0800

    云朝博客

--- wp-content/themes/wp-cms/templates/mainnav.php
+++ wp-content/themes/wp-cms/templates/mainnav.php
@@ -15,29 +15,41 @@
         });
     });
 </script>  
+<?php 
+    if (is_single())
+    {
+        $pid = get_the_ID();
+        $cat = get_the_category( $pid ); 
+        $cat_id = $cat[0]->term_id;
+    }
+    else
+    {
+        $cat_id = 0;
+    }
+?>
 <div id="navbar">
     <div id="navbar_right">
       <div id="menu">
-	  
+          
         <ul id="nav">
 
           <li><a href="<?php bloginfo('home'); ?>/" <?php if(is_home()) {echo 'class="class_cur nav_on"';} ?> ><span>首 页 </span></a></li>
           <li class="menu_line"/>
-          <li><a class="<?php if(is_category('20')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=20" target='_self'><span>资讯</span></a></li>
+          <li><a class="<?php if(is_category('20') || $cat_id == 20) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=20" target='_self'><span>资讯</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('1')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=1" target='_self'><span>事业</span></a></li>
+          <li><a class="<?php if(is_category('1') || $cat_id == 1) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=1" target='_self'><span>事业</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('10')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=10" target='_self'><span>生活</span></a></li>
+          <li><a class="<?php if(is_category('10') || $cat_id == 10) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=10" target='_self'><span>生活</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_single('444')) {echo 'class_cur nav_on';} ?>" href="http://yunchao.tuzhan.com/" target='_blank'><span>相册</span></a></li>
+          <li><a class="<?php if(is_single('444') || $cat_id == 444) {echo 'class_cur nav_on';} ?>" href="http://yunchao.tuzhan.com/" target='_blank'><span>相册</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('3')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=3" target='_self'><span>音乐</span></a></li>
+          <li><a class="<?php if(is_category('3') || $cat_id == 3) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=3" target='_self'><span>音乐</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('9')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=9" target='_self'><span>影视</span></a></li>
+          <li><a class="<?php if(is_category('9') || $cat_id == 9) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=9" target='_self'><span>影视</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('11')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=11" target='_self'><span>工具</span></a></li>
+          <li><a class="<?php if(is_category('11') || $cat_id == 11) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=11" target='_self'><span>工具</span></a></li>
           <li class='menu_line'></li>
-          <li><a class="<?php if(is_category('12')) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=12" target='_self'><span>资源</span></a></li>
+          <li><a class="<?php if(is_category('12') || $cat_id == 12) {echo 'class_cur nav_on';} ?>" href="<?php echo get_settings('home'); ?>/?cat=12" target='_self'><span>资源</span></a></li>
      
         </ul>
       </div>
