d30962d9c0c5a9e6a20bebb4051514ce604e0030
duanyunchao <duanyunchao@115.com>
2014-01-19 16:58:19 +0800

    云朝博客

---
+++ data/notsync/templates/compile/php.dyc.com/zh_CN/%%6D^6D7^6D7C5625%%test.tpl.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.22, created on 2013-11-16 19:42:56
+         compiled from test.tpl */ ?>
+测试<?php echo $this->_tpl_vars['return']['err_name']; ?>
+一下
---
+++ data/notsync/templates/compile/zh_CN/%%6D^6D7^6D7C5625%%test.tpl.php
@@ -0,0 +1,4 @@
+<?php /* Smarty version 2.6.22, created on 2014-01-19 16:56:45
+         compiled from test.tpl */ ?>
+测试<?php echo $this->_tpl_vars['return']['err_name']; ?>
+一下
